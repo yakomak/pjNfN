5873ca2158a21d32ae6e06798eea19c832350fb3
yakomak <yakomak137@gmail.com>
2017-04-03 16:05:15 +0300

    abrc 1

---
+++ servak.js
@@ -0,0 +1,11 @@
+var path = require("path");
+var express = require('express'),
+    app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname, "public/index.html"));
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3013);
